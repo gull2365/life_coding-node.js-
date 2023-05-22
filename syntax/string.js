e0418b3135d3cc315c703de5efed888456771ec2
Gull 2365 <jych2365@gmail.com>
2023-05-22 16:02:17 +0900

    feat: 6.2.JavaScript 문법 - String

---
+++ syntax/string.js
@@ -0,0 +1,5 @@
+console.log("1" + "1");
+console.log(
+  "All samples shall be properly labeled in order to ensure that the results can be traced back to the location from which it was collected."
+    .length
+);
