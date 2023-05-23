bf210f1f7943032c6665c5dea7effd38419dc064
Gull 2365 <jych2365@gmail.com>
2023-05-23 12:07:10 +0900

    feat: 10.URL을 통해서 입력된 값 사용하기

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 var http = require("http");
-var fs = require("fs");
+var fs = require("fs"); // node.js가 가지고 있는 수많은 기능을 그룹핑 해놓은것 module
+var url = require("url"); // url 모듈을 url이라는 이름으로 사용할 것이다라는 것을 의미
+
 var app = http.createServer(function (request, response) {
-  var url = request.url;
-  if (request.url == "/") {
-    url = "/index.html";
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  console.log(queryData.id);
+  if (_url == "/") {
+    _url = "/index.html";
   }
-  if (request.url == "/favicon.ico") {
-    response.writeHead(404);
-    response.end();
-    return;
+  if (_url == "/favicon.ico") {
+    return response.writeHead(404);
   }
   response.writeHead(200);
-  response.end(fs.readFileSync(__dirname + url));
+  response.end(queryData.id);
 });
 app.listen(3000);
