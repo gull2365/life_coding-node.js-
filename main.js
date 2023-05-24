00edda5d42bf829de213decfbf3cf1af9aa2288b
Gull 2365 <jych2365@gmail.com>
2023-05-24 23:35:28 +0900

    feat: 19.2.App 제작-홈페이지 구현

--- main.js
+++ main.js
@@ -9,31 +9,61 @@ var app = http.createServer(function (request, response) {
   var title = queryData.id;
 
   if (pathname === "/") {
-    fs.readFile(`data/${title}`, "utf8", function (err, description) {
-      var template = `
-  <!DOCTYPE html>
-<html>
+    if (title === undefined) {
+      fs.readFile(`data/${title}`, "utf8", function (err, description) {
+        var title = "Welcome";
+        var description = "Hello Node.js";
+        var template = `
+    <!DOCTYPE html>
+  <html>
   <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8" />
+  <title>WEB1 - ${title}</title>
+  <meta charset="utf-8" />
   </head>
   <body>
-    <h1><a href="/">WEB</a></h1>
-    <ol>
-      <li><a href="/?id=HTML">HTML</a></li>
-      <li><a href="/?id=CSS">CSS</a></li>
-      <li><a href="/?id=JAVASCRIPT">JavaScript</a></li>
-    </ol>
-    <h2>${title}</h2>
-    <p>
-      ${description}
-    </p>
+  <h1><a href="/">WEB</a></h1>
+  <ol>
+  <li><a href="/?id=HTML">HTML</a></li>
+  <li><a href="/?id=CSS">CSS</a></li>
+  <li><a href="/?id=JAVASCRIPT">JavaScript</a></li>
+  </ol>
+  <h2>${title}</h2>
+  <p>
+  ${description}
+  </p>
   </body>
-</html>
+  </html>
   `;
-      response.writeHead(200);
-      response.end(template);
-    });
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else {
+      fs.readFile(`data/${title}`, "utf8", function (err, description) {
+        var template = `
+    <!DOCTYPE html>
+  <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8" />
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      <ol>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JAVASCRIPT">JavaScript</a></li>
+      </ol>
+      <h2>${title}</h2>
+      <p>
+        ${description}
+      </p>
+    </body>
+  </html>
+    `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    }
   } else {
     response.writeHead(404);
     response.end("Not found");
