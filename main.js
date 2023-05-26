fc6de0c2773e853ad7a5e8e0f66c254c32ea169a
Gull 2365 <jych2365@gmail.com>
2023-05-26 22:19:55 +0900

    feat: 24.App 제작-글목록 출력하기

--- main.js
+++ main.js
@@ -1,67 +1,78 @@
 var http = require("http");
-var fs = require("fs"); // node.js가 가지고 있는 수많은 기능을 그룹핑 해놓은것 module
-var url = require("url"); // url 모듈을 url이라는 이름으로 사용할 것이다라는 것을 의미
+var fs = require("fs");
+var url = require("url");
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
-  var title = queryData.id;
-
   if (pathname === "/") {
-    if (title === undefined) {
-      fs.readFile(`data/${title}`, "utf8", function (err, description) {
+    if (queryData.id === undefined) {
+      fs.readdir("./data", function (error, filelist) {
         var title = "Welcome";
-        var description = "Hello Node.js";
+        var description = "Hello, Node.js";
+        var list = "<ul>";
+        var i = 0;
+        while (i < filelist.length) {
+          list =
+            list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+          i = i + 1;
+        }
+        list = list + "</ul>";
         var template = `
-    <!DOCTYPE html>
-  <html>
-  <head>
-  <title>WEB1 - ${title}</title>
-  <meta charset="utf-8" />
-  </head>
-  <body>
-  <h1><a href="/">WEB</a></h1>
-  <ol>
-  <li><a href="/?id=HTML">HTML</a></li>
-  <li><a href="/?id=CSS">CSS</a></li>
-  <li><a href="/?id=JAVASCRIPT">JavaScript</a></li>
-  </ol>
-  <h2>${title}</h2>
-  <p>
-  ${description}
-  </p>
-  </body>
-  </html>
-  `;
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
         response.writeHead(200);
         response.end(template);
       });
     } else {
-      fs.readFile(`data/${title}`, "utf8", function (err, description) {
-        var template = `
-    <!DOCTYPE html>
-  <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8" />
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ol>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JAVASCRIPT">JavaScript</a></li>
-      </ol>
-      <h2>${title}</h2>
-      <p>
-        ${description}
-      </p>
-    </body>
-  </html>
-    `;
-        response.writeHead(200);
-        response.end(template);
+      fs.readdir("./data", function (error, filelist) {
+        var title = "Welcome";
+        var description = "Hello, Node.js";
+        var list = "<ul>";
+        var i = 0;
+        while (i < filelist.length) {
+          list =
+            list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+          i = i + 1;
+        }
+        list = list + "</ul>";
+        fs.readFile(
+          `data/${queryData.id}`,
+          "utf8",
+          function (err, description) {
+            var title = queryData.id;
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+            </html>
+            `;
+            response.writeHead(200);
+            response.end(template);
+          }
+        );
       });
     }
   } else {
