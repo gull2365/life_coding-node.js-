97ffc8db3dd88c070a16419fb3f083eafaa90868
Gull 2365 <jych2365@gmail.com>
2023-05-28 18:46:41 +0900

    feat: 25.3.JavaScript-함수의 출력

--- syntax/function2.js
+++ syntax/function2.js
@@ -2,7 +2,8 @@ console.log(Math.round(1.6)); //2
 console.log(Math.round(1.4)); //1
 
 function sum(a, b) { // parameter
-  console.log(a + b);
+  console.log(a);
+  return a + b;
 }
 
-sum(2, 4); // argument
+console.log(sum(2, 4)); // argument
