e3236b5f8cd18135c7fcd6e0b3150d44f6ff22ad
Gull 2365 <jych2365@gmail.com>
2023-05-28 13:04:52 +0900

    feat: 25.1.JavaScript 함수의 기본 문법

---
+++ syntax/function.js
@@ -0,0 +1,15 @@
+function f123() {
+  console.log(1);
+  console.log(2);
+  console.log(3);
+}
+
+f123();
+console.log("A");
+console.log("B");
+console.log("C1");
+f123();
+console.log("C2");
+console.log("D");
+console.log("F");
+f123();
