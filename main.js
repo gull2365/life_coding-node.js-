bb4f8160a3c03cfe8c842337f09c76d58a34aa21
Gull 2365 <jych2365@gmail.com>
2023-06-02 20:06:49 +0900

    feat: 35.App 제작-글수정-수정할 정보 전송

--- main.js
+++ main.js
@@ -78,7 +78,7 @@ var app = http.createServer(function (request, response) {
       var template = templateHTML(
         title,
         list,
-        `<form action="http://localhost:3000/create_process" method="post">
+        `<form action=" /create_process" method="post">
         <p><input type="text" name="title" placeholder="title"></p>
         <p>
           <textarea name="description" placeholder="description"></textarea>
@@ -104,6 +104,30 @@ var app = http.createServer(function (request, response) {
       fs.writeFile(`data/${title}`, description, "utf8", function (err) {});
       response.writeHead(302, { location: `/?id=${title}` });
       response.end();
+    });
+  } else if (pathname === "/update") {
+    fs.readdir("./data", function (error, filelist) {
+      var list = templateList(filelist);
+      fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+        var title = queryData.id;
+        var template = templateHTML(
+          title,
+          list,
+          `<form action="update_process" method="post">
+          <input type="hidden" name="id" value="${title}">
+        <p><input type="text" name="title" placeholder="title" value=${title}></p>
+        <p>
+          <textarea name="description" placeholder="description">${description}</textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+      </form>`,
+          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+        );
+        response.writeHead(200);
+        response.end(template);
+      });
     });
   } else {
     response.writeHead(404);
